0546b5b554dc5918cc31127c288de5f9080a73f9
Sarvesh Gundi <sarvesh.gundi@gmail.com>
2026-09-02 14:24:35 +0530

    Reapply "InitialSetup"
    This reverts commit 60fb93063e1f6eb42c0e622be819d2d565ac97e5.

---
+++ HealthcarePro/HealthcarePro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthcarePro
+//
+//  Created by Sarvesh on 02/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
